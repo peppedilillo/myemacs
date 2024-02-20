e275e0d3cd81c5e2a82c41d10164fd7dc54aaf7e
peppedilillo <peppedilillo@gmail.com>
2024-02-21 00:50:45 +0100

    some aestheticsssss

---
+++ splash-screen.el
@@ -0,0 +1,147 @@
+;;; splash-screen.el --- An alternative splash screen -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020 Nicolas .P Rougier
+
+;; Author: Nicolas P. Rougier <nicolas.rougier@inria.fr>
+;; URL: https://github.com/rougier/emacs-splash
+;; Keywords: startup
+;; Version: 0.1
+;; Package-Requires: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;;  An alternative splash screen:
+;;
+;;  +–—————————––––––––––––––––––––––––––––————————————————————+
+;;  |                                                          |
+;;  |                                                          |
+;;  |                                                          |
+;;  |                                                          |
+;;  |                                                          |
+;;  |                                                          |
+;;  |                       www.gnu.org                        |
+;;  |                  GNU Emacs version XX.Y                  |
+;;  |                   a free/libre editor                    |
+;;  |                                                          |
+;;  |                                                          |
+;;  |                                                          |
+;;  |                                                          |
+;;  |                                                          |
+;;  |        GNU Emacs comes with ABSOLUTELY NO WARRANTY       |
+;;  |     Copyright (C) 2020 Free Software Foundation, Inc.    |
+;;  |                                                          |
+;;  +––––––––––––––––––––––––––––––––––––––————————————————————+
+;;
+;; Features:
+;;
+;;  - No logo, no moddeline, no scrollbars
+;;  - "q" or <esc> kills the splash screen
+;;  - Any other key open the about-emacs buffer
+;;  - With emacs-mac (Mituharu), splash screen is faded out after 3 seconds
+;;
+;; Note: The screen is not shown if there are opened file buffers. For
+;;       example, if you start emacs with a filename on the command
+;;       line, the splash is not shown.
+;;
+;; Usage:
+;; 
+;;  (require 'splash-screen)
+;;
+;;; Code:
+(require 'cl-lib)
+
+
+(defun splash-screen ()
+  "Emacs splash screen"
+  
+  (interactive)
+  (let* ((splash-buffer  (get-buffer-create "*splash*"))
+         (height         (- (window-body-height nil) 1))
+         (width          (window-body-width nil))
+         (padding-center (- (/ height 2) 1))
+         (padding-bottom (- height (/ height 2) 3)))
+
+    ;; If there are buffer associated with filenames,
+    ;;  we don't show splash screen.
+    (if (eq 0 (length (cl-loop for buf in (buffer-list)
+                              if (buffer-file-name buf)
+                              collect (buffer-file-name buf))))
+        
+        (with-current-buffer splash-buffer
+          (erase-buffer)
+          
+          ;; Buffer local settings
+          (if (one-window-p)
+              (setq mode-line-format nil))
+          (setq cursor-type nil)
+          (setq vertical-scroll-bar nil)
+          (setq horizontal-scroll-bar nil)
+          (setq fill-column width)
+          (face-remap-add-relative 'link :underline nil)
+
+          ;; Vertical padding to center
+          (insert-char ?\n padding-center)
+
+          ;; Central text
+          (insert-text-button " www.gnu.org "
+                     'action (lambda (_) (browse-url "https://www.gnu.org"))
+                     'help-echo "Visit www.gnu.org website"
+                     'follow-link t)
+          (center-line) (insert "\n")
+          (insert (concat
+                   (propertize "GNU Emacs"  'face 'bold)
+                   " " "version "
+                   (format "%d.%d" emacs-major-version emacs-minor-version)))
+          (center-line) (insert "\n")
+          (insert (propertize "A free/libre editor" 'face 'shadow))
+          (center-line)
+
+
+          ;; Vertical padding to bottom
+          (insert-char ?\n padding-bottom)
+
+          ;; Copyright text
+          (insert (propertize
+                   "GNU Emacs comes with ABSOLUTELY NO WARRANTY" 'face 'shadow))
+          (center-line) (insert "\n")
+          (insert (propertize
+                   "Copyright (C) 2020 Free Software Foundation, Inc." 'face 'shadow))
+          (center-line) (insert "\n")
+
+          (goto-char 0)
+          (read-only-mode t)
+          
+          (display-buffer-same-window splash-buffer nil)))))
+
+;; Suppress any startup message in the echo area
+(run-with-idle-timer 0.05 nil (lambda() (message nil)))
+
+;; Install hook after frame parameters have been applied and only if
+;; no option on the command line
+(if (and (not (member "-no-splash"  command-line-args))
+         (not (member "--file"      command-line-args))
+         (not (member "--insert"    command-line-args))
+         (not (member "--find-file" command-line-args))
+         (not inhibit-startup-screen)
+         )
+    (progn
+      (add-hook 'window-setup-hook 'splash-screen)
+      (setq inhibit-startup-screen t 
+            inhibit-startup-message t
+            inhibit-startup-echo-area-message t)))
+
+(provide 'splash-screen)
+;;; splash-screen.el ends here
