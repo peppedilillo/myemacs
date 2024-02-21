2f65bfa06782261ae553a17aa90a2f3933287c73
peppedilillo <peppedilillo@gmail.com>
2024-02-21 01:24:57 +0100

    some more modding

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 (require 'package)
 (package-initialize)
 
+;; active packages
+(setq package-selected-packages '(haskell-mode doom-themes))
+
 ;; aesthetics
 (load-theme 'doom-manegarm t)
 (set-face-attribute 'default nil :font "Monaco-14")
@@ -44,17 +47,3 @@
 (setq c-default-style "linux"
       c-basic-offset 4)
 
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(haskell-mode doom-themes)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
