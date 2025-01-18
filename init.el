9ad60754c7961476b8f5145326ab91f44c02bfcf
peppedilillo <peppedilillo@gmail.com>
2025-01-19 00:32:33 +0100

    some refactoring, improved python config

--- init.el
+++ init.el
@@ -9,25 +9,20 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'gruber-darker t)
 
-;; all themes are considered safe
-(setq custom-safe-themes t)
-
-;; sets font specs
-(set-face-attribute 'default nil :height 130)
+;; sets font
+(set-face-attribute 'default nil :font "JetBrains Mono-13" :weight `extralight)
+(setq-default line-spacing 0.2)
 
 ;; disables context menubar, toolbar, scrollbar
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
 
-;; pixel level smooth scrolling
-(pixel-scroll-precision-mode 1)
-
-;; cua mode
-(cua-mode t)
-
-;: fuck-offs custom variables to separate file
-(setq custom-file (concat user-emacs-directory "/custom.el"))
+;; load custom splash screen
+(load "~/.emacs.d/loads/splash-screen.el")
+(require 'splash-screen)
+;; or if you grow tired, this disables the splash screen
+;; (setq inhibit-startup-screen t)
 
 ;; switch audio bell for visual one
 (setq visible-bell nil
@@ -39,11 +34,14 @@
     (run-with-timer (* 2 flash-sec) nil #'invert-face 'mode-line)
     (run-with-timer (* 3 flash-sec) nil #'invert-face 'mode-line)))
 
-;; load custom splash screen
-(load "~/.emacs.d/loads/splash-screen.el")
-(require 'splash-screen)
-;; or if you grow tired, this disables the splash screen
-;; (setq inhibit-startup-screen t)
+;; all themes are considered safe
+(setq custom-safe-themes t)
+
+;; pixel level smooth scrolling
+(pixel-scroll-precision-mode 1)
+
+;: fuck-offs custom variables to separate file
+(setq custom-file (concat user-emacs-directory "/custom.el"))
 
 ;; backups to backups dir
 (setq backup-directory-alist
@@ -57,6 +55,9 @@
 (setq lock-file-name-transforms
       '(("\\`/.*/\\([^/]+\\)\\'" "~/.emacs.d/backups/\\1" t)))
 
+;; cua mode
+(cua-mode t)
+
 ;; enables electric pair mode
 (electric-pair-mode 1)
 
@@ -66,14 +67,44 @@
 ;; automatically enables line numbers in prog mode
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
-;; olivetti write mode
-(use-package olivetti
-  :ensure t)
-
 ;; company completion
 (use-package company
   :ensure t
   :hook (after-init . global-company-mode))
+
+;; typographic ligatures.
+(defconst jetbrains-ligatures
+   '("-->" "//" "/**" "/*" "*/" "<!--" ":=" "->>" "<<-" "->" "<-"
+     "<=>" "==" "!=" "<=" ">=" "=:=" "!==" "&&" "||" "..." ".."
+     "|||" "///" "&&&" "===" "++" "--" "=>" "|>" "<|" "||>" "<||"
+     "|||>" "<|||" ">>" "<<" "::=" "|]" "[|" "{|" "|}"
+     "[<" ">]" ":?>" ":?" "/=" "[||]" "!!" "?:" "?." "::"
+     "+++" "??" "###" "##" ":::" "####" ".?" "?=" "=!=" "<|>"
+     "<:" ":<" ":>" ">:" "<>" "***" ";;" "/==" ".=" ".-" "__"
+     "=/=" "<-<" "<<<" ">>>" "<=<" "<<=" "<==" "<==>" "==>" "=>>"
+     ">=>" ">>=" ">>-" ">-" "<~>" "-<" "-<<" "=<<" "---" "<-|"
+     "<=|" "/\\" "\\/" "|=>" "|~>" "<~~" "<~" "~~" "~~>" "~>"
+     "<$>" "<$" "$>" "<+>" "<+" "+>" "<*>" "<*" "*>" "</>" "</" "/>"
+     "<->" "..<" "~=" "~-" "-~" "~@" "^=" "-|" "_|_" "|-" "||-"
+     "|=" "||=" "#{" "#[" "]#" "#(" "#?" "#_" "#_(" "#:" "#!" "#="
+     "&="))
+
+(use-package ligature
+  :ensure t
+  :config
+  ;; Enable the "www" ligature in every possible major mode
+  (ligature-set-ligatures 't '("www"))
+  ;; Enable traditional ligature support in eww-mode, if the
+  ;; `variable-pitch' face supports it
+  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+  ;; Enable all Cascadia Code ligatures in programming modes
+  (ligature-set-ligatures 'prog-mode jetbrains-ligatures)
+  ;; Enables ligature checks globally in all buffers.
+  (global-ligature-mode t))
+
+;; olivetti write mode
+(use-package olivetti
+  :ensure t)
 
 ;; loads machine-specific init file, if present
 (defvar mac-custom-file "~/.emacs.d/init_mac.el")
